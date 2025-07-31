fed6708cd9c7b8f7cee7f64b18a13c4158fe087a
hezpaty <hezpaty07@gmail.com>
2025-07-31 15:20:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,103 +20,110 @@ const { WebSocket, createWebSocketStream } = require('ws');
 const NAME = process.env.NAME || os.hostname();
 
 const clientVlessConfig = {
-  "dns": {
-    "servers": ["localhost"]
-  },
-  "inbounds": [
-    {
-      "listen": "0.0.0.0",
-      "port": "1080",
-      "protocol": "dokodemo-door",
-      "settings": {
-        "network": "tcp,udp",
-        "followRedirect": true,
-        "address": "127.0.0.1"
-      },
-      "tag": "tun-inbound"
-    },
-    {
-      "listen": "127.0.0.1",
-      "port": 10808,
-      "protocol": "socks",
-      "settings": {
-        "auth": "noauth",
-        "udp": true
-      },
-      "tag": "socks-inbound"
-    }
-  ],
-  "outbounds": [
-    {
-      "mux": { "enabled": false },
-      "protocol": "vless",
-      "proxySettings": {
-        "tag": "fbproxy",
-        "transportLayer": true
-      },
-      "settings": {
-        "vnext": [
-          {
-            "address": "ge0.ggpht.com",
-            "port": 443,
-            "users": [
-              {
-                "id": "aaaaaaa1-bbbb-2ccc-accc-eeeeeeeeeee3",
-                "level": 8,
-                "encryption": "none"
-              }
-            ]
-          }
-        ]
-      },
-      "streamSettings": {
-        "network": "ws",
-        "security": "tls",
-        "tlsSettings": {
-          "allowInsecure": true,
-          "serverName": "ge0.ggpht.com"
-        },
-        "wsSettings": {
-          "headers": {
-            "Host": "hezpaty-281713011959.us-west1.run.app"
-          },
-          "path": "/topnet"
-        }
-      },
-      "tag": "VLESS"
-    },
-    {
-      "domainStrategy": "AsIs",
-      "protocol": "http",
-      "settings": {
-        "servers": [
-          {
-            "address": "ge0.ggpht.com",
-            "port": 443
-          }
-        ],
-        "headers": {
-          "Host": "hezpaty-281713011959.us-west1.run.app",
-          "Proxy-Connection": "keep-alive",
-          "User-Agent": "Mozilla/5.0 (Linux; Android 14; SM-A245F Build/UP1A.231005.007; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/133.0.6943.122 Mobile Safari/537.36 [FBAN/InternetOrgApp;FBAV/166.0.0.0.169;]",
-          "X-iorg-bsid": "@hezpaty"
-        }
-      },
-      "tag": "fbproxy"
-    }
-  ],
-  "policy": {
-    "levels": {
-      "8": {
-        "connIdle": 300,
-        "downlinkOnly": 1,
-        "handshake": 4,
-        "uplinkOnly": 1
-      }
-    }
-  }
+  "dns": {
+    "servers": [
+      "localhost"
+    ]
+  },
+  "inbounds": [
+    {
+      "listen": "0.0.0.0",
+      "port": "1080",
+      "protocol": "dokodemo-door",
+      "settings": {
+        "network": "tcp,udp",
+        "followRedirect": true,
+        "address": "127.0.0.1"
+      },
+      "tag": "tun-inbound"
+    },
+    {
+      "listen": "127.0.0.1",
+      "port": 10808,
+      "protocol": "socks",
+      "settings": {
+        "auth": "noauth",
+        "udp": true
+      },
+      "tag": "socks-inbound"
+    }
+  ],
+  "outbounds": [
+    {
+      "mux": {
+        "enabled": false
+      },
+      "protocol": "vless",
+      "proxySettings": {
+        "tag": "hezpaty",
+        "transportLayer": true
+      },
+      "settings": {
+        "vnext": [
+          {
+            "address": "yt3.ggpht.com",
+            "port": 443,
+            "users": [
+              {
+                "id": "aaaaaaa1-bbbb-2ccc-accc-eeeeeeeeeee3",
+                "level": 8,
+                "encryption": "none"
+              }
+            ]
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws",
+        "security": "tls",
+        "tlsSettings": {
+          "allowInsecure": true,
+          "serverName": "yt3.ggpht.com"
+        },
+        "wsSettings": {
+          "headers": {
+            "Host": "hezpaty-281713011959.us-west1.run.app"
+          },
+          "path": "/topnet"
+        }
+      },
+      "tag": "VLESS"
+    },
+    {
+      "domainStrategy": "AsIs",
+      "protocol": "http",
+      "settings": {
+        "servers": [
+          {
+            "address": "57.144.138.4",
+            "port": 8080
+          },
+          {
+            "address": "157.240.195.32",
+            "port": 8080
+          }
+        ],
+        "headers": {
+          "Host": "social.keezy.site:443",
+          "Proxy-Connection": "keep-alive",
+          "User-Agent": "Mozilla/5.0 (Linux; Android 14; SM-A245F Build/UP1A.231005.007; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/133.0.6943.122 Mobile Safari/537.36 [FBAN/InternetOrgApp;FBAV/166.0.0.0.169;]",
+          "X-iorg-bsid": "@hezpaty"
+        }
+      },
+      "tag": "hezpaty"
+    }
+  ],
+  "policy": {
+    "levels": {
+      "8": {
+        "connIdle": 300,
+        "downlinkOnly": 1,
+        "handshake": 4,
+        "uplinkOnly": 1
+      }
+    }
+  }
 };
-
 const UUID = "aaaaaaa1-bbbb-2ccc-accc-eeeeeeeeeee3";
 const DOMAIN = "hezpaty-281713011959.us-west1.run.app";
 const PORT = process.env.PORT || 8080;
@@ -134,7 +141,6 @@ const httpServer = http.createServer((req, res) => {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Hello, TOpNeT Customs\n');
     } else if (req.url === `/${UUID}`) {
-        const vlessURL = `vless://${UUID}@ge0.ggpht.com:443?encryption=none&security=tls&sni=ge0.ggpht.com&type=ws&host=${DOMAIN}&path=%2Ftopnet#Vl-ws-tls-${NAME}`;
         const vlessURL = `vless://${UUID}@yt3.ggpht.com:443?encryption=none&security=tls&sni=yt3.ggpht.com&type=ws&host=${DOMAIN}&path=%2Ftopnet#Vl-ws-tls-${NAME}`;
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end(vlessURL + '\n');

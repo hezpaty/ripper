028fd89c3ffe725d3f44071afb10c32ddba9e94b
hezpaty <hezpaty07@gmail.com>
2025-08-01 23:19:36 +0300

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,24 @@
+{
+  "inbounds": [
+    {
+      "port": 8080,
+      "protocol": "vless",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "aaaaaaa1-bbbb-2ccc-accc-eeeeeeeeeee3"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}
